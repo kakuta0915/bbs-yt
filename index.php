9d8c9150ade2d1c783ead4873c2821a98d1c9ba2
kakuta0915 <kakuta19960915@gmail.com>
2024-01-10 18:12:49 +0900

    雛形作成

--- index.php
+++ index.php
@@ -1,1 +1,14 @@
 <?php echo "Hello PHP"; ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="style.css">
+  <title>PHP 掲示板</title>
+</head>
+<body>
+  
+</body>
+</html>
