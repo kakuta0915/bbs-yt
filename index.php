b0f06e3e9f367f3fb42c9652c1d038743de0c747
kakuta0915 <kakuta19960915@gmail.com>
2024-01-10 18:30:20 +0900

    コメント部分の情報を追加

--- index.php
+++ index.php
@@ -10,6 +10,19 @@
   <h1 class="title">PHP 掲示板アプリ</h1>
   <hr>
   <div class="boardWrapper">
+    <section>
+      <article>
+        <div class="wrapper">
+          <div class="nameArea">
+            <span>名前: </span>
+            <p class="username">kakauta0915</p>
+            <time>: 2024/9/15</time>
+          </div>
+          <p class="comment">手書きコメントです。</p>
+        </div>
+      </article>
+    </section>
+
     <form class="formWrapper">
       <div>
         <input type="submit" value="書き込む">
